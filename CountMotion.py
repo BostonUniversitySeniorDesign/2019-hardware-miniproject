c2379cff01cdfeb6b33b4a824c3a4eca39bc0a89
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-09-04 15:43:20 -0400

    functionalize

--- CountMotion.py
+++ CountMotion.py
@@ -18,6 +18,7 @@ config_fn = Path(__file__).parent / "config.ini"
 # use plots to empirically adjust
 ilanes = [(25, 27), (35, 40)]
 max_cumulative = 100
+max_psd = 1000
 
 
 def main():
@@ -53,48 +54,27 @@ def main():
     # %% create figure
     CarCount = np.zeros(time.size, dtype=int)
     j = 0
-    if doplot:
-        fg = figure(figsize=(8, 10))
-        ax1, ax2, ax3 = fg.subplots(3, 1)
-        fg.suptitle("spatial FFT car counting")
-
-        h1 = ax1.imshow(mot[0], origin="upper")
-        ht1 = ax1.set_title("")
-        # plot lanes
-        ax1.axhline(ilanes[0][0], color="cyan", linestyle="--")
-        ax1.axhline(ilanes[0][1], color="cyan", linestyle="--")
-        ax1.axhline(ilanes[1][0], color="orange", linestyle="--")
-        ax1.axhline(ilanes[1][1], color="orange", linestyle="--")
-
-        ax3.set_title("cumulative car count")
-        ax3.set_xlabel("elapsed time (seconds)")
-        ax3.set_ylabel("count")
-        ax3.grid(True)
-        ax3.set_ylim(0, max_cumulative)
-        h3, = ax3.plot(time, CarCount)
-
-        fg.tight_layout()
-        fg.canvas.draw()
-    else:
-        fg = ax1 = ax2 = None
+    L = mot.shape[-1]
+    iLPF = (int(L * 4 / 9), int(L * 5.2 / 9))
+    h = fig_create(doplot, mot[0], iLPF, detect_min, detect_max, time, CarCount)
     # %% main program loop over each frame of motion data
     for i, m in enumerate(bmot):
         # %% process each frame
-        N = spatial_discrim(m, detect_min, detect_max, ax2)
+        N = spatial_discrim(m, iLPF, detect_min, detect_max, h)
         if i % frame_count_interval == 0:
             j += 1
             CarCount[j] = N
             if doplot:
-                h3.set_ydata(np.cumsum(CarCount))
+                h["h3"].set_ydata(np.cumsum(CarCount))
         # %% save plots
         if doplot:
-            ht1.set_text(f"h.264 difference frames: index {i}, elapsed seconds {time[j]}")
-            h1.set_data(mot[i])
-            fg.canvas.draw()
-            fg.canvas.flush_events()
+            h["t1"].set_text(f"h.264 difference frames: index {i}, elapsed seconds {time[j]}")
+            h["h1"].set_data(mot[i])
+            h["fg"].canvas.draw()
+            h["fg"].canvas.flush_events()
             pause(0.001)
             if p.saveplot:
-                fg.savefig(p.saveplot + f"{i:05d}.png", bbox_inches="tight", dpi=100)
+                h["fg"].savefig(p.saveplot + f"{i:05d}.png", bbox_inches="tight", dpi=100)
     # %% write car counts to disk
     if p.outfn is not None:
         outfn = Path(p.outfn).expanduser()
@@ -103,16 +83,14 @@ def main():
             f["count"] = CarCount
 
 
-def spatial_discrim(mot: np.ndarray, detect_min: float, detect_max: float, ax2=None) -> int:
+def spatial_discrim(mot: np.ndarray, iLPF: typing.Tuple[int, int],
+                    detect_min: float, detect_max: float, h: dict) -> int:
     """
     implement spatial LPF for two lanes of traffic
     """
     # %% define two spatial lanes of traffic
-    lane1 = mot[ilanes[0][0]: ilanes[0][1], :].sum(axis=0)
-    lane2 = mot[ilanes[1][0]: ilanes[1][1], :].sum(axis=0)
-    # %% setup rectangular spatial LPF for each lane -- cars are big
-    L = lane1.size
-    iLPF = (int(L * 4 / 9), int(L * 5.2 / 9))
+    lane1 = mot[ilanes[0][0]:ilanes[0][1], :].sum(axis=0)
+    lane2 = mot[ilanes[1][0]:ilanes[1][1], :].sum(axis=0)
     # %% motion PSD
     Flane1 = np.fft.fftshift(abs(np.fft.fft(lane1)) ** 2)
     Flane2 = np.fft.fftshift(abs(np.fft.fft(lane2)) ** 2)
@@ -120,26 +98,59 @@ def spatial_discrim(mot: np.ndarray, detect_min: float, detect_max: float, ax2=N
     N1 = int(detect_min <= Flane1[iLPF[0]: iLPF[1]].sum() <= detect_max)
     N2 = int(detect_min <= Flane2[iLPF[0]: iLPF[1]].sum() <= detect_max)
     # %% plot
-    plotlanes(N1, N2, L, Flane1, Flane2, iLPF, ax2)
+    h["h21"].set_ydata(Flane1)
+    h["h22"].set_ydata(Flane2)
 
     return N1 + N2
 
 
-def plotlanes(N1: int, N2: int, L: int, Flane1: np.ndarray, Flane2: np.ndarray, iLPF: typing.Tuple[int, int], ax2):
-    if ax2 is None:
-        return
+def fig_create(doplot: bool, img: np.ndarray,
+               iLPF: typing.Tuple[int, int],
+               detect_min: float, detect_max: float,
+               time: typing.Sequence[float],
+               CarCount: typing.Sequence[int]) -> dict:
 
-    ax2.cla()
-    fx = range(-L // 2, L // 2)
-    ax2.plot(fx, Flane1)
-    ax2.plot(fx, Flane2)
-    ax2.set_title(f"lane counts {N1} {N2}")
-    ax2.set_ylim(0, 1000)
+    if not doplot:
+        return {}
+
+    fg = figure(figsize=(8, 10))
+    ax1, ax2, ax3 = fg.subplots(3, 1)
+    fg.suptitle("spatial FFT car counting")
+
+    h = {"fg": fg,
+         "h1": ax1.imshow(img, origin="upper"),
+         "t1": ax1.set_title("")}
+    # plot lanes
+    ax1.axhline(ilanes[0][0], color="cyan", linestyle="--")
+    ax1.axhline(ilanes[0][1], color="cyan", linestyle="--")
+    ax1.axhline(ilanes[1][0], color="orange", linestyle="--")
+    ax1.axhline(ilanes[1][1], color="orange", linestyle="--")
+
+    L = img.shape[-1]
+    fx = np.arange(-L // 2, L // 2)
+    h["h21"], = ax2.plot(fx, [0]*fx.size)
+    h["h22"], = ax2.plot(fx, [0]*fx.size)
+    ax2.set_title("Spatial frequency")
+    ax2.set_ylim(0, max_psd)
     ax2.set_xlabel("Spatial Frequency bin (arbitrary units)")
     ax2.set_ylabel("magnitude$^2$")
-    # indicate LPF bounds
+    # %% setup rectangular spatial LPF for each lane -- cars are big
     ax2.axvline(iLPF[0] - L // 2, color="red", linestyle="--")
     ax2.axvline(iLPF[1] - L // 2, color="red", linestyle="--")
+    ax2.axhline(detect_min, linestyle="--")
+    ax2.axhline(detect_max, linestyle="--")
+
+    ax3.set_title("cumulative car count")
+    ax3.set_xlabel("elapsed time (seconds)")
+    ax3.set_ylabel("count")
+    ax3.grid(True)
+    ax3.set_ylim(0, max_cumulative)
+    h["h3"], = ax3.plot(time, CarCount)
+
+    fg.tight_layout()
+    fg.canvas.draw()
+
+    return h
 
 
 if __name__ == "__main__":
