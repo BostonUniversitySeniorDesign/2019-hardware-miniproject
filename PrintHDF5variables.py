1db56288e3ad562f48fb582c144136b59e3423a2
Michael Hirsch, Ph.D <10931741+scivision@users.noreply.github.com>
2019-09-25 14:58:59 -0400

    more robust HDF5 variable printing

--- PrintHDF5variables.py
+++ PrintHDF5variables.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Prints HDF5 variable names. Also works for Matlab 7.3 .mat files
+"""
 import h5py
 import argparse
 from pathlib import Path
@@ -10,7 +13,13 @@ P = p.parse_args()
 
 
 def print_key(name: str, obj):
-    print(name, obj.dtype, obj.shape)
+    if obj.file.filename.endswith(".mat"):
+        if "#refs#" in obj.name:
+            return
+    try:
+        print(name, obj.dtype, obj.shape)
+    except AttributeError:
+        pass
 
 
 with h5py.File(Path(P.filename).expanduser()) as f:
